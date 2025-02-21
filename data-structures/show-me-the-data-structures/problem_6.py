e58e95651b3b7e34f4f70c403f8ff553db46cfdf
Pablo Ávila <pdaa.900910@gmail.com>
2025-02-21 11:17:00 +0100

    solution of problem 6

--- data-structures/show-me-the-data-structures/problem_6.py
+++ data-structures/show-me-the-data-structures/problem_6.py
@@ -122,10 +122,32 @@ def union(llist_1: LinkedList, llist_2: LinkedList) -> LinkedList:
         A new linked list containing all unique elements from both input linked lists.
     """
     # Use a set to store all unique elements
-    pass
+    firs_set = set()
+    second_set = set()
+    if llist_1.head is not None:
+        node = llist_1.head
+        while node:
+            firs_set.add(node.value)
+            node = node.next
+    if llist_2.head is not None:
+        node = llist_2.head
+        while node:
+            second_set.add(node.value)
+            node = node.next
 
+    result_set = set()
+    #add all elements from first set to the result
+    for element_first_set in firs_set:
+        result_set.add(element_first_set)
+    #add all elements from second set to the result
+    for element_second_set in second_set:
+        result_set.add(element_second_set)
     # Create a new linked list to store the union
-    pass
+    result = LinkedList()
+    for element in result_set:
+        result.append(element)
+    return result
+
 
 def intersection(llist_1: LinkedList, llist_2: LinkedList) -> LinkedList:
     """
@@ -144,13 +166,28 @@ def intersection(llist_1: LinkedList, llist_2: LinkedList) -> LinkedList:
         A new linked list containing all elements that are present in both input linked lists.
     """
     # Use sets to find the intersection
-    pass
+    firs_set = set()
+    second_set = set()
+    if llist_1.head is not None:
+        node = llist_1.head
+        while node:
+            firs_set.add(node.value)
+            node = node.next
+    if llist_2.head is not None:
+        node = llist_2.head
+        while node:
+            second_set.add(node.value)
+            node = node.next
+
+    result = LinkedList()
 
     # Find the intersection of both sets
-    pass
+    for element_first_set in firs_set:
+        for element_second_set in second_set:
+            if element_first_set == element_second_set:
+                result.append(element_first_set)
 
-    # Create a new linked list to store the intersection
-    pass
+    return result
 
 if __name__ == "__main__":
     ## Test case 1
@@ -187,8 +224,33 @@ if __name__ == "__main__":
     print("Union:", union(linked_list_3, linked_list_4)) # Expected: 1, 2, 3, 4, 6, 7, 8, 9, 11, 21, 23, 35, 65
     print("Intersection:", intersection(linked_list_3, linked_list_4)) # Expected: empty
 
-    ## Test case 3
-    pass
+    ## Test case 3 -  empty linked list Union
+    print("Test Case 3:empty linked list Union")
+    linked_list_5 = LinkedList()
+    linked_list_6 = LinkedList()
+    print("Union:", union(linked_list_5, linked_list_6)) # Expected: empty
 
-    ## Test case 4
-    pass
+    ## Test case 4 -  empty linked list Intersection
+    print("Test Case 3:empty linked list Intersection")
+    linked_list_5 = LinkedList()
+    linked_list_6 = LinkedList()
+    print("Intersection:", intersection(linked_list_5, linked_list_6))  # Expected: empty
+
+    ## Test case 5 - Union one empty and other not
+    print("Test Case 3:Union one empty and other not")
+    linked_list_5 = LinkedList()
+    element_2 = [1, 7, 8, 9, 11, 21, 1]
+    for i in element_2:
+        linked_list_5.append(i)
+    linked_list_6 = LinkedList()
+    print("Union:", union(linked_list_5, linked_list_6))
+
+    #Test case 6 - Union and Intersection with same elements
+    print("#Test case 6 - Union and Intersection with same elements")
+    llist_1 = LinkedList()
+    llist_2 = LinkedList()
+    for i in [1, 2, 3]:
+        llist_1.append(i)
+        llist_2.append(i)
+    print("Union:", union(llist_1, llist_2))  # Expected: 1, 2, 3
+    print("Intersection", intersection(llist_1, llist_2))  # Expected: 1, 2, 3
